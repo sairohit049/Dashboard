f70d096d1570f0d5b7491cf2c7180734e72dfa5a
sairohit049 <sairohitvurukuti@gmail.com>
2026-07-14 17:47:13 +0530

    Updated fetch URL

--- user.js
+++ user.js
@@ -1,6 +1,6 @@
 async function getdata() {
     let con=document.getElementById("container")
-    let res=await fetch("http://localhost:3000/cars")
+    let res=await fetch("https://dashboard-vi12.onrender.com/cars")
     let data=await res.json()
     data.forEach(ele => {
         let x=document.createElement("div")
